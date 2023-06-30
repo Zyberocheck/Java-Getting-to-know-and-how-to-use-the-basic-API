e32cfe6ee9dfb3ebf224714297ba7556b3672ec8
Alexandr <sasha-bragin@mail.ru>
2023-07-01 00:00:29 +0300

    Java2

--- DZ3/DZ_3.java
+++ DZ3/DZ_3.java
@@ -1,33 +1,61 @@
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.Iterator;
 import java.util.Random;
 
 public class DZ_3 {
     public static void main(String[] args){
-        randomNumbers(20,100);
+// 1) Нужно удалить из него чётные числа
+        // randomNumbers(10,100);
+// 2) Найти минимальное максимальное и среднее( по всей видимости среднее арифметическое) значение
+        // minMaxMid(10,100);
+
 
     }
-    static void randomNumbers(int size, int maxValue){
-        ArrayList<Integer> nums = new ArrayList<>(size);
-        ArrayList<Integer> numsa = new ArrayList<>(size);
 
-        Random rand = new Random();
+    // static void randomNumbers(int size, int maxValue){
+    //     ArrayList<Integer> nums = new ArrayList<>(size);
+    //     Random rand = new Random();
         
-        for (int i = 0; i < size; i++)
-            nums.add(rand.nextInt(maxValue));
+    //     for (int i = 0; i < size; i++)
+    //         nums.add(rand.nextInt(maxValue));
 
-        for (Integer num : nums)
-            System.out.print(num + " ");
-        System.out.println();
-
-        for (Integer num : nums)
-            if(num % 2!=0)
-            numsa.add(num);
-            System.out.print(numsa + "- массив без четных чисел");
-        System.out.println();
+    //     System.out.println(nums.toString());
+        
+    //     for (int i = 0; i < nums.size()-1; i++) {
+    //         if(nums.get(i) % 2 == 0){
+    //         nums.remove(nums.get(i));
+    //         i--;
+    //         }           
+    //     }
+    //     System.out.println(nums.toString());
+    // }    
     
-    }
+    // static void minMaxMid (int size, int value){
+    //     ArrayList<Integer> nums = new ArrayList<>(size);
+    //     Random rand = new Random();
         
+    //     for (int i = 0; i < size; i++)
+    //         nums.add(rand.nextInt(value));
+
+    //     System.out.println(nums.toString());            
+
+    //     int min = Collections.min(nums);
+    //     System.out.printf("Минимальное число в списке %d \n", min);
+
+    //     int max = Collections.max(nums);
+    //     System.out.printf("Максимальное число в списке %d \n", max);         
+
+    //     double sum =0;
+    //     for (int i = 0; i < nums.size(); i++) {
+    //         sum = nums.get(i)+sum;  
+    //     }
+    //     System.out.println(sum);
+    //     System.out.println("Среднее арифметическое чисел списка:" + sum / size);         
+        
+    // }
+    
+
 }
 
