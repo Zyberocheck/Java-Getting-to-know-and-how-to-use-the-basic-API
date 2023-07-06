96114fcdbe9b6589e85452858759fdff7e94dcae
Alexandr <sasha-bragin@mail.ru>
2023-07-07 00:15:52 +0300

    DZ4

--- DZ4/DZ4.java
+++ DZ4/DZ4.java
@@ -8,14 +8,14 @@ import java.util.Scanner;
 public class DZ4 {
     public static void main(String[] args) {
         // Умножение
-        Deque<Integer> a = createDeque(12);
-        Deque<Integer> b = createDeque(1);
+        Deque<Integer> a = createDeque(22);
+        Deque<Integer> b = createDeque(18);
 
         System.out.println(multi(a, b));
 
         // Сложение
-        Deque<Integer> x = createDeque(-532);
-        Deque<Integer> y = createDeque(123);
+        Deque<Integer> x = createDeque(-734);
+        Deque<Integer> y = createDeque(236);
 
         System.out.println(sum(x, y));
 
@@ -24,9 +24,9 @@ public class DZ4 {
     static LinkedList<Integer> sum(Deque<Integer> a, Deque<Integer> b) {
         int sum = 0;
         int signX = a.getLast() / Math.abs(a.getLast());
-        System.out.println(signX);
+        
         int signY = b.getLast() / Math.abs(b.getLast());
-        System.out.println(signY);
+        
         if (a.size() >= b.size()) {
             for (int i = 0; i <= a.size() - b.size(); i++) {
                 b.addLast(0);
@@ -37,21 +37,17 @@ public class DZ4 {
                 a.addLast(0);
             }
         }
-        System.out.println(a);
-        System.out.println(b);
 
         int i = 0;
         while (!a.isEmpty()) {
             int x = a.pollFirst() * (int) Math.pow(10, i);
-            System.out.println("Это Х " + x);
-            System.out.println();
+            
             int y = b.pollFirst() * (int) Math.pow(10, i);
-            System.out.println("Это У " + y);
-            System.out.println();
+            
             sum += (Math.abs(x) * signX) + (Math.abs(y) * signY);
             i++;
-            System.out.println("Это sum " + sum);
-            System.out.println();
+            
+            
         }
 
         return linkedRes(sum);
@@ -90,10 +86,10 @@ public class DZ4 {
         while (x != 0) {
             if (x < 10 && x > -10) {
                 res.addFirst(x % 10);
-                System.out.println("Это res1 " + res);
+
             } else {
                 res.addFirst(Math.abs(x) % 10);
-                System.out.println("Это res2 " + res);
+
             }
             x /= 10;
         }
